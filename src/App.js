8e47d09796ba1c25422ccf80721764c2176ad38a
Biraj <birajparajuli2001@gmail.com>
2021-07-16 10:09:58 +0545

    Initial changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Football Ecommerce Website [React]
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Birajparajuli/football-site-project-III"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Account
         </a>
       </header>
     </div>
