1626930e611c9adcb8811df1edba902490cd2b25
Biraj <birajparajuli2001@gmail.com>
2021-07-16 19:41:39 +0545

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Football Ecommerce Website [React]
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Birajparajuli/football-site-project-III"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Account
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import '../css/components.css';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className="main-nav">
+                <h1>This is navbar</h1>
+            </div>
+
+        )
+    }
+}
