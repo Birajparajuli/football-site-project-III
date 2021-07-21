d44513211e2396a245bd1bc4ded8bf745183976f
Biraj <birajparajuli2001@gmail.com>
2021-07-21 07:49:15 +0545

    1 slider image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './css/App.css';
 import Navbar from './components/Navbar';
+import ImgSlider from './components/ImgSlider';
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <ImgSlider />
     </>
   )
 }
