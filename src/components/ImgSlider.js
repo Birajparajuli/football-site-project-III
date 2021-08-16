30f41ee06326acb65052465d98e358affc50efae
Biraj <birajparajuli2001@gmail.com>
2021-08-16 10:40:03 +0545

    slider 1

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import '../css/components.css';
+import Slider11 from '../img/slider/Slider11.jpg';
+export default class ImgSlider extends Component {
+
+    render() {
+        return (
+            <>
+                <img src={Slider11} alt="slider-1" />
+            </>
+        )
+
+    }
+
+}
