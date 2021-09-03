821ec95ebbaddb3ccd5315b4d7221622b25e446f
Biraj <birajparajuli2001@gmail.com>
2021-09-03 22:10:07 +0545

    slider improvements

---
+++ src/data/ProductData.js
@@ -0,0 +1,10 @@
+import image1 from '../img/product/Messi_PSG.jpeg';
+export const ProductData = [
+    {
+        name:'Messi jersey PSG',
+        price:'500',
+        image:image1,
+        alt:'messi-jersy',
+        category:'men',
+    }
+] 
