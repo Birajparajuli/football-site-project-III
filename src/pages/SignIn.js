324a94be12e563e6db3ba6c6d6c8f444d4e3eced
Biraj <birajparajuli2001@gmail.com>
2021-09-17 20:43:26 +0545

    sign-up form added

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,51 +1,48 @@
 import React from "react";
 import "./Style/signIn.css";
 
+import { FcGoogle } from "react-icons/fc";
 const SignIn = () => {
   return (
     <div className="sign-in-container">
-      <div className="sign-in-info">
-        <h2>Welcome to</h2>
-        <h1>FootballHub</h1>
-      </div>
-
-      <div className="form-container">
+      <form className="sign-up-form">
         <h3>Create an Account</h3>
         <div className="google-login">
-          <button className="btn">SIGN IN WITH GOOGLE</button>
+          <button className="btn google-btn">
+            <FcGoogle size="1.5em" />
+            SIGN IN WITH GOOGLE
+          </button>
         </div>
         <div className="or-bar">
           <span className="line"></span>
           <h4>Or</h4>
           <span className="line"></span>
         </div>
-        <form>
-          <h3>Sign in with E-mail</h3>
-          <div className="form-items">
-            <label htmlFor="name">Username</label>
-            <input type="text" id="name" placeholder="Username" />
-            <small>Error Message</small>
-          </div>
-          <div className="form-items">
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" placeholder="Email" />
-            <small>Error Message</small>
-          </div>
-          <div className="form-items">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password-1" placeholder="Password" />
-            <small>Error Message</small>
-          </div>
-          <div className="form-items">
-            <label htmlFor="password">Confirm Password</label>
-            <input type="password" id="password-2" placeholder="Password" />
-            <small>Error Message</small>
-          </div>
-          <button type="submit" className="btn">
-            Submit
-          </button>
-        </form>
-      </div>
+        <h3>Sign in with E-mail</h3>
+        <div className="form-items">
+          <label htmlFor="name">Username</label>
+          <input type="text" id="name" placeholder="Username" />
+          <small>Error Message</small>
+        </div>
+        <div className="form-items">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Email" />
+          <small>Error Message</small>
+        </div>
+        <div className="form-items">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password-1" placeholder="Password" />
+          <small>Error Message</small>
+        </div>
+        <div className="form-items">
+          <label htmlFor="password">Confirm Password</label>
+          <input type="password" id="password-2" placeholder="Password" />
+          <small>Error Message</small>
+        </div>
+        <button type="submit" className="btn signup-btn">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
