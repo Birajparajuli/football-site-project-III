a38b2f97fae8f77520ea9f45c00c5e42437f02e6
Biraj <birajparajuli2001@gmail.com>
2021-09-26 19:38:46 +0545

    slider images fix

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -2,11 +2,12 @@ import image1 from "../img/slider/messi.png";
 import image2 from "../img/slider/ronaldo.png";
 import image3 from "../img/slider/ramos.png";
 import image4 from "../img/slider/haland.png";
+import image5 from "../img/slider/neymar.png";
 
 export const SliderData = [
   {
     title: "Messi jersey PSG",
-    price: "Rs. 5000",
+    price: "Rs. 5999",
     path: "/homes",
     image: image1,
     alt: "Messi Jersey",
@@ -14,15 +15,31 @@ export const SliderData = [
   },
   {
     title: "Ronaldo Jersey MU",
-    price: "Rs. 5000",
+    price: "Rs. 4999",
     path: "/homes",
     image: image2,
     alt: "Ronaldo Jersey",
     label: "View Product",
   },
   {
+    title: "Neymar Jersey PSG",
+    price: "Rs. 4525",
+    path: "/homes",
+    image: image5,
+    alt: "Messi Jersey",
+    label: "Learn More",
+  },
+  {
+    title: "Halland Jersey BVB",
+    price: "Rs. 4525",
+    path: "/homes",
+    image: image4,
+    alt: "Messi Jersey",
+    label: "Learn More",
+  },
+  {
     title: "Ramos Jersey PSG",
-    price: "Rs. 5000",
+    price: "Rs. 4525",
     path: "/homes",
     image: image3,
     alt: "Messi Jersey",
